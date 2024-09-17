94349f637c55eea7aa8addb4f5ade55302136c96
Subhrajit Talukdar <117500946+subhrajit77@users.noreply.github.com>
2024-09-17 12:07:55 +0530

    added hero section component

--- sites/Home/HomePage.jsx
+++ sites/Home/HomePage.jsx
@@ -1,9 +1,13 @@
 import Navbar from '../../components/global/Navbar/Navbar';
+import Hero from '../../components/home/Hero/Hero';
+
+import "./Homepage.css";
 
 export default function HomePage() {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 }
