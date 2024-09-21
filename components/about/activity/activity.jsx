417c9a9e407e310088130532f9fbdd927a0e94fc
Abhishek Kashyap <abhik5567@gmail.com>
2024-09-21 15:46:42 +0530

    add club activities

---
+++ components/about/activity/activity.jsx
@@ -0,0 +1,53 @@
+import "./activity.css";
+
+
+export default function Activity() {
+    return (
+        <div className="activity-container">
+            <h2 className="heading">
+                Club <span>Activities</span> and <span>programs</span>
+            </h2>
+            <div className="row">
+
+                <div className="card card-circle">
+                    <div className="card-body">
+                        <img src="./assets/activities/seminar.png" alt="" />
+                        <h5 className="card-title">Workshops & Seminars</h5>
+                        <p className="card-text">
+                            Our club organizes regular workshops and seminars
+                            conducted by industry experts, covering topics such
+                            as ideation, business planning, marketing
+                            strategies, and more
+                        </p>
+                    </div>
+                </div>
+                <div className="card card-circle">
+                    <div className="card-body">
+                        <img src="./assets/activities/competition.png" alt="" />
+                        <h5 className="card-title">
+                            Entrepreneurship Competitions
+                        </h5>
+                        <p className="card-text">
+                            We host exciting entrepreneurship competitions where
+                            students get to pitch their business ideas, receive
+                            valuable feedback, and compete for prizes.
+                        </p>
+                    </div>
+                </div>
+                <div className="card card-circle">
+                    <div className="card-body">
+                        <img src="./assets/activities/funding.png" alt="" />
+                        <h5 className="card-title">Startup Incubator</h5>
+                        <p className="card-text">
+                            Our club provides support for members with promising
+                            startup ideas, offering access to mentorship,
+                            funding opportunities, and a collaborative space to
+                            grow their ventures.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    );
+}
